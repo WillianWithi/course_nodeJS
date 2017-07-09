b7a2050a1bd0dfa828d76ed63f294407b6fbec39
willian <willianwithi@hotmail.com>
2017-07-09 20:32:14 -0300

    importando lib http, e levantando servidor na pasta 3000

--- course/noticias.js
+++ course/noticias.js
@@ -1,1 +1,14 @@
-console.log('Criando um site de noticias com NodeJs');
+// importando lib http
+var http = require('http');  
+
+/* criar um servidor
+ * @param  requisicao = realiza uma requisicao
+ * @param  resposta = resposta do serviidor
+ */
+
+var server = http.createServer(function (requisicao,resposta){
+	resposta.end("<html><body>Portal de noticias</body></html>");
+});
+
+// setar porta do servidor
+server.listen(3000);
